889bbb52435ce920d9e82485fa9ce65d093fb276
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2023-03-08 18:41:35 +0100

    Add sign up

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    
+    # flash.now[:notice] = "hi man"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'main#index'
 
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
   get 'about', to: 'about#index'
 end
